dde185565c23aae57c2f0764bc118e7eb8d87fa7
Mathieu Gravey <mathieu@mgravey.com>
2022-03-29 16:02:12 +0200

    Update timeseries.py

--- timeseries.py
+++ timeseries.py
@@ -14,16 +14,16 @@ def printErrorMessage(task_id,errorMessage,adviceMessage='Double check the input
 
 class GP_timeseries_v1(GEE_Service):
 
-	def reduce2aPixel(self,im):
-		return im.addBands(self.ee.Image.constant(self.ee.Number(im.get('system:time_start')).divide(1000)).rename('time').toLong()).sample(region=self.ee.Geometry.Point([lon, lat]), scale=10, numPixels=1);
+	def reduce2aPixel(self,im,coordinates):
+		return im.addBands(self.ee.Image.constant(self.ee.Number(im.get('system:time_start')).divide(1000)).rename('time').toLong()).sample(region=self.ee.Geometry.Point(coordinates), scale=10, numPixels=1);
 
-	def boundingTimeCollection(self,timeStart,timeEnd):
+	def boundingTimeCollection(self,timeStart,timeEnd,coordinates):
 		ERA5_pressur=self.ee.ImageCollection("ECMWF/ERA5_LAND/HOURLY").filterDate(timeStart,ee.Date(timeEnd).advance(1,'hour')).select(['surface_pressure'],['pressure']);
-		fc=ERA5_pressur.map(self.reduce2aPixel).flatten()
+		fc=ERA5_pressur.map(self.reduce2aPixel,coordinates).flatten()
 		url=self.ee.FeatureCollection(fc).getDownloadURL(selectors=['time','pressure'])
 		return url;
 
-	def expliciteTimeCollection(self,time,pressure):
+	def expliciteTimeCollection(self,time,pressure,coordinates):
 		def makeFeature(li):
 			li=self.ee.List(li);
 			return self.ee.Feature(None,{'system:time_start':self.ee.Number(li.get(0)).multiply(1000),'pressure':li.get(1)})
@@ -53,7 +53,7 @@ class GP_timeseries_v1(GEE_Service):
 
 			altIm=self.ee.Image('projects/earthimages4unil/PostDocProjects/rafnuss/Geopot_ERA5');
 			dh = self.ee.Image(ft.get('bestERA5')).select('temperature_2m').divide(Lb).multiply(self.ee.Image.constant(self.ee.Number(ft.get('pressure'))).divide(self.ee.Image(ft.get('bestERA5')).select('surface_pressure')).pow(-R*Lb/g0/M).subtract(1)).add(altIm).rename('altitude');
-			return dh.addBands(self.ee.Image(ft.get('bestERA5')).select('surface_pressure').rename('pressure')).addBands(self.ee.Image.constant(self.ee.Number(ft.get('system:time_start'))).rename('time').divide(1000).toLong()).sample(region=self.ee.Geometry.Point([lon, lat]), scale=10, numPixels=1);
+			return dh.addBands(self.ee.Image(ft.get('bestERA5')).select('surface_pressure').rename('pressure')).addBands(self.ee.Image.constant(self.ee.Number(ft.get('system:time_start'))).rename('time').divide(1000).toLong()).sample(region=self.ee.Geometry.Point(coordinates), scale=10, numPixels=1);
 
 		agregatedMap=self.ee.FeatureCollection(era5_llabelFeature.map(getAltitude)).flatten();
 		url=agregatedMap.getDownloadURL(selectors=['time','pressure','altitude'])
@@ -68,12 +68,12 @@ class GP_timeseries_v1(GEE_Service):
 			return printErrorMessage(timeStamp,'lon and lat are mendatory!')
 
 		try:
-			lon=float(form.getvalue("lon"));
+			lon=float(form["lon"][0]);
 		except:
 			return printErrorMessage(timeStamp,'lon is not a float number');
 
 		try:
-			lat=float(form.getvalue("lat"));
+			lat=float(form["lat"][0]);
 		except:
 			return printErrorMessage(timeStamp,'lat is not a float number');
 
@@ -103,18 +103,10 @@ class GP_timeseries_v1(GEE_Service):
 
 		try:
 			if informedTimeSeries:
-				url=self.expliciteTimeCollection(time,pressure);
+				url=self.expliciteTimeCollection(time,pressure,[lon, lat]);
 			else:
-				url=self.boundingTimeCollection(timeStart,timeEnd);
-			dic = {'status':'success', 'taskID':timeStamp,'data':dic};
-			return (200,{"Content-type":"application/json"},json.JSONEncoder().encode(dic));
-		except Exception as e:
-			return printErrorMessage(timeStamp,str(e),"An error has occurred. Please try again, and if the problem persists, file an issue on https://github.com/Rafnuss/GeoPressureServer/issues/new?body=task_id:{}&labels=crash".format(timeStamp));
-
-
-
-		try:
+				url=self.boundingTimeCollection(timeStart,timeEnd,[lon, lat]);
 			dic = {'status':'success', 'taskID':timeStamp,'data':{'format':'csv','url':url}};
-			return (200,{"Content-type":"application/json"},json.JSONEncoder().encode(result));
+			return (200,{"Content-type":"application/json"},json.JSONEncoder().encode(dic));
 		except Exception as e:
 			return printErrorMessage(timeStamp,str(e),"An error has occurred. Please try again, and if the problem persists, file an issue on https://github.com/Rafnuss/GeoPressureServer/issues/new?body=task_id:{}&labels=crash".format(timeStamp));
