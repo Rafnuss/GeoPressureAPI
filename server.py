82ecc6c3e352f9cd706e0218467ab13e32fa5465
Mathieu Gravey <mathieu@mgravey.com>
2022-03-29 14:47:48 +0200

    convert using the new template

--- server.py
+++ server.py
@@ -1,13 +1,21 @@
-import http.server
- 
+#!/usr/bin/env python3
+import os
+from GEE_API_server import GEE_Handler, GEE_server
+from map import *
+from timeseries import *
+
 PORT = 24853
 server_address = ("", PORT)
 
-server = http.server.HTTPServer
-handler = http.server.CGIHTTPRequestHandler
-handler.cgi_directories = ["/","/GeoPressure/v1/","/GeoPressure"]
-handler.path='glp.mgravey.com'
+server = GEE_server
+handler = GEE_Handler;
 print("Serveur actif sur le port :", PORT)
 
-httpd = server(server_address, handler)
+#["/","/GeoPressure/v1/","/GeoPressure"]
+httpd = server(server_address, handler,{'/GeoPressure/v1/timeseries/':GP_timeseries_v1(os.environ['GEE_API_ADDRESS'],'../gee-api-key.json'),
+										'/GeoPressure/v1/map/':GP_map_v1(os.environ['GEE_API_ADDRESS'],'../gee-api-key.json')})
+#Dont need httpS
+# httpd.socket = ssl.wrap_socket (httpd.socket,
+#         keyfile="../ssl/privkey.pem",
+#         certfile='../ssl/fullchain.pem', server_side=True)
 httpd.serve_forever()
