9d8160d49b983451d99730a08310e172f064cda0
Mathieu Gravey <mathieu@mgravey.com>
2023-01-03 15:25:27 +0100

    add highvolume endpoint

--- gpServer.py
+++ gpServer.py
@@ -1,21 +1,43 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.9
 import os
 from GEE_API_server import GEE_Handler, GEE_server
 from map import *
 from timeseries import *
+import os
 
-PORT = 24853
+PORT = 24854
 server_address = ("", PORT)
+
+os.makedirs("./logs", exist_ok = True) 
 
 server = GEE_server
 handler = GEE_Handler;
 print("Serveur actif sur le port :", PORT)
 
+hightVolumeEndpoint=True;
+
 #["/","/GeoPressure/v1/","/GeoPressure"]
-httpd = server(server_address, handler,{'/GeoPressure/v1/timeseries/':GP_timeseries_v1(os.environ['GEE_API_ADDRESS'],'../gee-api-key.json'),
-										'/GeoPressure/v1/map/':GP_map_v1(os.environ['GEE_API_ADDRESS'],'../gee-api-key.json')})
-#Dont need httpS
-# httpd.socket = ssl.wrap_socket (httpd.socket,
-#         keyfile="../ssl/privkey.pem",
-#         certfile='../ssl/fullchain.pem', server_side=True)
+httpd = server(server_address, handler,{'/GeoPressure/v1/timeseries/':GP_timeseries_v1(os.environ['GEE_API_ADDRESS'],'../gee-api-key.json',hightVolumeEndpoint),
+										'/GeoPressure/v1/map/':GP_map_v1(os.environ['GEE_API_ADDRESS'],'../gee-api-key.json',hightVolumeEndpoint)})
+#https is handle by nginx
 httpd.serve_forever()
+
+## nginx config 
+
+# server {
+#     listen 80;
+#     listen 24853;
+#     listen 443 ssl http2;
+#     fastcgi_read_timeout 1200;
+#     proxy_read_timeout 1200;
+#     server_name DOMAIN_NAME;
+
+#     ssl_certificate /etc/letsencrypt/live/**/fullchain.pem; # managed by Certbot
+#     ssl_certificate_key /etc/letsencrypt/live/**/privkey.pem; # managed by Certbot
+
+#     include /etc/letsencrypt/options-ssl-nginx.conf;
+
+#     location / {
+#         proxy_pass       http://localhost:24854;
+#     }
+# }
