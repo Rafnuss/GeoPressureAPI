9348ff1cbf6ccc615c13fa1f0e76eb9b700a5135
Mathieu Gravey <mathieu@mgravey.com>
2025-11-27 20:18:20 +0100

    Update pressurePath.py

--- pressurePath/pressurePath.py
+++ pressurePath/pressurePath.py
@@ -219,9 +219,9 @@ class GP_pressurePath(GEE_Service):
 
             # Select appropriate ERA5 dataset
             if dataset.lower() == "single-levels":
-                ERA5 = self.ee.ImageCollection("ECMWF/ERA5/HOURLY")
+                ERA5 = era5_single
             elif dataset.lower() == "land":
-                ERA5 = self.ee.ImageCollection("ECMWF/ERA5_LAND/HOURLY")
+                ERA5 = era5_land
             else:
                 ERA5 = self.ERA5Combined
 
